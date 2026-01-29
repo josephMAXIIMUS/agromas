e5fa861c45a2abb125a9e2144570e26031627b48
joseph pedraza leo <88262486+josephMAXIIMUS@users.noreply.github.com>
2026-01-29 10:31:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from sqlalchemy.types import String
 from urllib.parse import quote_plus
 from dotenv import load_dotenv
 import os
-load_dotenv() 
+load_dotenv()
 app = FastAPI()
-apikey=os.getenv("AGROMAS_API_KEY")
+
 
 
 def funcion_connect():
@@ -29,7 +29,7 @@ def funcion_connect():
 
 @app.get("/")
 def raiz():
-    return {apikey}
+    return {"mensaje": "¡Bienvenido a la API principal!"}
 
 
 
@@ -38,11 +38,11 @@ def raiz():
 def leer_datos(fecha: str, formato: int, nombre_tabla_db: str):
     from dotenv import load_dotenv
     try:
-       
-     
+
+
         # 1️⃣ Obtener datos de la API externa
         url = "https://agromas.safcoperu.pe:9443/packing/web/agrodigital/datos-registrados"
-        
+
         params = {"fecha": fecha, "id_formato": formato}
         apikey=os.getenv("AGROMAS_API_KEY")
         headers = {"X-Agromas-Apikey": apikey}
@@ -54,7 +54,7 @@ def leer_datos(fecha: str, formato: int, nombre_tabla_db: str):
 
         datos_leidos = pd.DataFrame(registros, columns=criterios)
         numero_de_filas = len(datos_leidos)
-    
+
         dtype_sql = {}
         for col in datos_leidos.columns:
             if datos_leidos[col].dtype == 'object':
@@ -66,8 +66,8 @@ def leer_datos(fecha: str, formato: int, nombre_tabla_db: str):
         nuevo = funcion_connect()
         # print(list(df_head.columns))
         # print(list(datos_leidos.columns))
-        
-        
+
+
         df_head.to_sql(nombre_tabla_db, con=nuevo, schema='AGROMAS', if_exists='append',dtype=dtype_sql, index=False,method='multi')
 
 
@@ -78,8 +78,8 @@ def leer_datos(fecha: str, formato: int, nombre_tabla_db: str):
             f"WHERE FECHA = '{fecha}'"
         )
         fecha_db = pd.read_sql(consulta, engine_original)
-    
-        
+
+
         fecha_registro= fecha_db['fecha_registrada'].iloc[0]
         fecha_registro_1=str(fecha_registro)
         var_fecha_1 = str(fecha)
@@ -101,12 +101,11 @@ def leer_datos(fecha: str, formato: int, nombre_tabla_db: str):
                         #append
                         #replace
                 #print(datos_leidos)
-                return("datos ingresados")          
+                return("datos ingresados")
             except Exception as e:
                     print("❌ Error al obtener o insertar datos:", e)
-        return fecha,formato,nombre_tabla_db, a
+        return fecha,formato,nombre_tabla_db
 
     except Exception as e:
         # Devuelve error como mensaje
         raise HTTPException(status_code=500, detail=f"❌ Error al obtener o insertar datos: {e}")
-
